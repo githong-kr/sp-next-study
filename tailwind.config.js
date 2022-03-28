c349f20a8b9b84f14da6edfc6bbeb3959d056028
bale <githong@kakao.com>
2022-03-28 23:44:30 +0900

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    /* 현재 프로젝트의 특정 경로의 하위 모든 폴더,
+     ** 특정 확장자의 모든 파일에서
+     ** Tailwind 를 사용한 걸 찾아낸다. */
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'media', //class
+  plugins: [],
+};
