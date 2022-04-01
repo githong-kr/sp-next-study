dbf3512e66ba3b0c9703677e1c06682363dbcde6
bale <githong@kakao.com>
2022-04-01 23:00:37 +0900

    add client.ts

---
+++ lib/server/client.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+const getNewPrismaClient = () => {
+  const newPrismaClient = new PrismaClient({
+    log: [{ emit: 'event', level: 'query' }],
+  });
+
+  newPrismaClient.$on('query', (e) => {
+    // Query Logging
+    console.log('Query: ' + e.query);
+    console.log('Params: ' + e.params);
+    console.log('Duration: ' + e.duration + 'ms');
+  });
+
+  return newPrismaClient;
+};
+
+const client = global.client || getNewPrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  global.client = client;
+}
+
+export default client;
